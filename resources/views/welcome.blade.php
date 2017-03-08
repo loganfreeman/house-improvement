83a30713c430fa7d1bb6749299b7eecf2497f419
shanhong cheng <shanhong.cheng@utah.edu>
2017-03-08 12:32:58 -0700

    admin link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -61,7 +61,9 @@
         <div class="top-right links">
             @if (Auth::check())
                 <a href="{{ url('/home') }}">Home</a>
-                <a href="{{ url('/admin') }}">Admin</a>
+                @if (Voyager::can('browse_admin'))
+                  <a href="{{ url('/admin') }}">Admin</a>
+                @endif
             @else
                 <a href="{{ url('/login') }}">Login</a>
                 <a href="{{ url('/register') }}">Register</a>
